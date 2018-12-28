2d9217e44585d3b639a7200be9965661a208d576
Unknown <laura-h-95@live.com.au>
2018-12-28 12:22:52 +1000

    Add custom type declarations for spark library (incomplete)

---
+++ @types/ciscospark/index.d.ts
@@ -0,0 +1,91 @@
+declare module 'ciscospark/env' {
+	interface WebhookObject {
+		id ?: string;
+		resource ?: string;
+		event ?: string;
+		filter ?: string;
+		targetUrl? : string;
+		name ?: string;
+		created ?: Date;
+	}
+
+	interface RoomObject {
+		id ?: string;
+		title ?: string;
+		teamId ?: string;
+		created ?: Date;
+	}
+
+	interface MessageObject {
+		id ?: string;
+		personId ?: string;
+		personEmail ?: string;
+		roomId ?: string;
+		text ?: string;
+		markdown ?: string;
+		files ?: string[];
+		created ?: Date;
+	}
+
+	interface PersonObject {
+		id : string;
+		emails : string[];
+		displayName : string;
+		created : Date;
+	}
+
+	class Page<T> implements Iterable<T> {
+		items : T[];
+		length : number;
+		links : any;
+		spark : any;
+		static parseLinkHeaders : (linkHeaders : string) => any;
+		next : () => Promise<Page<T>>;
+		hasNext : () => boolean;
+		previous : () => Promise<Page<T>>;
+		hasPrevious : () => boolean;
+		getLink : (link : string) => Promise<Page<T>>;
+		hasLink : (link : string) => boolean;
+		[Symbol.iterator] : () => Iterator<T>;
+	}
+
+	class CiscoSpark {
+		ciscospark : boolean;
+		version : string;
+		webhooks : {
+			create : (webhook : WebhookObject) => Promise<WebhookObject>;
+			get : (webhook : WebhookObject | string) => Promise<WebhookObject[]>;
+			list : (options ?: {max : number}) => Promise<Page<WebhookObject>>;
+			remove : (webhook : WebhookObject | string) => Promise<void>;
+			update : (webhook : WebhookObject) => Promise<WebhookObject>;
+		};
+		messages : {
+			create : (message : MessageObject) => Promise<MessageObject>;
+			get : (room : RoomObject | string) => Promise<MessageObject>;
+			list : (options ?: {roomId : string, max : number}) => Promise<Page<MessageObject>>;
+			remove : (message : MessageObject | number) => Promise<void>;
+		};
+		rooms : {
+			create : (room : RoomObject) => Promise<RoomObject>;
+			get : (room : RoomObject | string, options : any) => Promise<RoomObject>;
+			list : (options ?: {max : number}) => Promise<Page<RoomObject>>;
+			remove : (room : RoomObject | string) => Promise<void>;
+			update : (room : RoomObject) => Promise<RoomObject>;
+		};
+		people : {
+			get : (person : PersonObject | number | string) => Promise<PersonObject>;
+			list : (options : number[] | {email : string, displayName : string}) => Promise<Page<PersonObject>>;
+		}
+	}
+
+	const spark : CiscoSpark;
+	export default spark;
+
+	export {
+		WebhookObject,
+		RoomObject,
+		MessageObject,
+		Page,
+		PersonObject
+	}
+}
