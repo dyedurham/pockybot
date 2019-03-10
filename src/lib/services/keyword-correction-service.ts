4d051f3f211380af8d7f3d93fdc1ba8fd0aa15d2
Blake McCririck <blake.mccririck@globalx.com.au>
2019-03-11 07:46:00 +1000

    start of key handling

--- src/lib/services/keyword-correction-service.ts
+++ src/lib/services/keyword-correction-service.ts
@@ -1,16 +1,29 @@
 import constants from '../../constants';
 import __logger from '../logger';
+import { emitKeypressEvents } from 'readline';
+
+
 
 export interface KeywordCorrectionService {
 	CorrectMessage(body: string, params: string) : Promise<string>
 }
 
 export class DefaultKeywordCorrectionService implements KeywordCorrectionService {
+    keys: string[];
+
+    constructor() {
+		this.keys = [];
+	}
+
 	async CorrectMessage(body: string, params: string) : Promise<string> {
         let html = "you peg has been succesfully sent";
         
         
 
 		return html;
-	}
+    }
+    
+    AddKey(key : string) : void {
+        this.keys.push(key);
+    }
 }
