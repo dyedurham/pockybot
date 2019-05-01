6944db7214fc809da50b5cffab918f3c062b0a48
Laura Berger <Laura.Berger@globalx.com.au>
2019-05-01 16:07:28 +1000

    Fix dockerfile and improve explanations around what a rotation is

--- src/lib/response-triggers/help.ts
+++ src/lib/response-triggers/help.ts
@@ -50,7 +50,7 @@ export default class Help extends Trigger {
 	1. To get a welcome message from me, type \`@${constants.botName} welcome\` OR direct message me with \`welcome\`.
 	1. I will respond in the room you messaged me in.
 * Check the rotation!
-	1. To check the rotation, type \`@${constants.botName} rotation\` OR direct message me with \`rotation\`.
+	1. To check the rotation of teams responsible for buying snacks, type \`@${constants.botName} rotation\` OR direct message me with \`rotation\`.
 	1. I will respond in the room you messaged me in.\n`;
 
 		if (this.config.checkRole(message.personId, Role.Admin) || this.config.checkRole(message.personId, Role.Winners)) {
