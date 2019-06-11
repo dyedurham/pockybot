ceff2eceb92183628576ceada86f67d388bae780
Laura Berger <Laura.Berger@globalx.com.au>
2019-06-12 09:34:25 +1000

    Fix implementation

--- src/lib/responder.ts
+++ src/lib/responder.ts
@@ -1,18 +1,19 @@
-import { MessageObject } from 'ciscospark/env';
+import { MessageObject, PersonObject } from 'ciscospark/env';
 const spark = require('ciscospark/env');
 import responseFactory from './response-triggers/index';
 import __logger from './logger';
 
 async function respond(messageEvent : {data : {id : string}}) {
-	try {		
+	try {
 		let message : MessageObject = await spark.messages.get(messageEvent.data.id);
 		__logger.debug('processing message: ' + JSON.stringify(message));
 		let room = message.roomId;
 
-		if (message.personEmail.includes('@sparkbot.io') || message.personEmail.includes('@webex.bot')) {
+		let person : PersonObject = await spark.people.get(message.personId);
+		if (person.type === 'bot') {
 			__logger.debug('Message was sent by a bot, ignoring this message.');
+			return;
 		}
-
 
 		let responseMessage : MessageObject;
 		try {
