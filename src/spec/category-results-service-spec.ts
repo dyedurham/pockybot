357fad5778122d7698c8925fdace3f46df1db97f
Jim <james.cullen@globalx.com.au>
2019-07-10 09:28:12 +1000

    fix category results service tests

--- src/spec/category-results-service-spec.ts
+++ src/spec/category-results-service-spec.ts
@@ -71,9 +71,9 @@ describe('category results service', () => {
 		categories = createCategoriesData();
 	});
 
-	xit('should generate the correct html', async (done: DoneFn) => {
-		var html = await categoryResultsService.returnCategoryResultsTable(results, categories);
-		html = html.replace(/>\s+</g, '><'); //remove whitespace to make testing easier
+	it('should generate the correct html', async (done: DoneFn) => {
+		let html = await categoryResultsService.returnCategoryResultsTable(results, categories);
+		html = html.replace(/>\s+</g, '><'); // remove whitespace to make testing easier
 
 		expect(html).toContain('<h2 class="clickable collapsed" data-toggle="collapse" ' +
 			'data-target="#section-categoryresults-0" aria-expanded="false" aria-controls="section-categoryresults-0">' +
@@ -81,14 +81,14 @@ describe('category results service', () => {
 			'<table id="section-categoryresults-0" class="table pb-3 collapse">' +
 			'<thead class="thead-light clickable" data-toggle="collapse" ' +
 			'data-target="#section-categoryresults-0-0" aria-expanded="true" aria-controls="section-categoryresults-0-0">' +
-			'<tr><th colspan="3"><i class="fas fa-plus"></i><i class="fas fa-minus"></i> receiver 2 &mdash; 2 peg(s) total</th></tr>' +
+			'<tr><th colspan="3"><i class="fas fa-plus"></i><i class="fas fa-minus"></i> receiver 2 &mdash; 2 pegs total</th></tr>' +
 			'</thead><tbody id="section-categoryresults-0-0" class="collapse show">' +
 			'<tr><td>sender 1</td><td>test brave</td><td>brave</td></tr>' +
 			'<tr><td>sender 2</td><td>test shame brave</td><td>shame, brave</td></tr>' +
 			'</tbody>' +
 			'<thead class="thead-light clickable" data-toggle="collapse" ' +
 			'data-target="#section-categoryresults-0-1" aria-expanded="true" aria-controls="section-categoryresults-0-1">' +
-			'<tr><th colspan="3"><i class="fas fa-plus"></i><i class="fas fa-minus"></i> receiver 1 &mdash; 1 peg(s) total</th></tr>' +
+			'<tr><th colspan="3"><i class="fas fa-plus"></i><i class="fas fa-minus"></i> receiver 1 &mdash; 1 peg total</th></tr>' +
 			'</thead><tbody id="section-categoryresults-0-1" class="collapse show">' +
 			'<tr><td>sender 2</td><td>test awesome brave</td><td>awesome, brave</td></tr>' +
 			'</tbody></table>');
@@ -99,7 +99,7 @@ describe('category results service', () => {
 			'<table id="section-categoryresults-1" class="table pb-3 collapse">' +
 			'<thead class="thead-light clickable" data-toggle="collapse" ' +
 			'data-target="#section-categoryresults-1-0" aria-expanded="true" aria-controls="section-categoryresults-1-0">' +
-			'<tr><th colspan="3"><i class="fas fa-plus"></i><i class="fas fa-minus"></i> receiver 1 &mdash; 2 peg(s) total</th></tr>' +
+			'<tr><th colspan="3"><i class="fas fa-plus"></i><i class="fas fa-minus"></i> receiver 1 &mdash; 2 pegs total</th></tr>' +
 			'</thead><tbody id="section-categoryresults-1-0" class="collapse show">' +
 			'<tr><td>sender 1</td><td>test awesome</td><td>awesome</td></tr>' +
 			'<tr><td>sender 2</td><td>test awesome brave</td><td>awesome, brave</td></tr>' +
@@ -111,7 +111,7 @@ describe('category results service', () => {
 			'<table id="section-categoryresults-2" class="table pb-3 collapse">' +
 			'<thead class="thead-light clickable" data-toggle="collapse" ' +
 			'data-target="#section-categoryresults-2-0" aria-expanded="true" aria-controls="section-categoryresults-2-0">' +
-			'<tr><th colspan="3"><i class="fas fa-plus"></i><i class="fas fa-minus"></i> receiver 2 &mdash; 1 peg(s) total</th></tr>' +
+			'<tr><th colspan="3"><i class="fas fa-plus"></i><i class="fas fa-minus"></i> receiver 2 &mdash; 1 peg total</th></tr>' +
 			'</thead><tbody id="section-categoryresults-2-0" class="collapse show">' +
 			'<tr><td>sender 2</td><td>test shame brave</td><td>shame, brave</td></tr>' +
 			'</tbody></table>');
